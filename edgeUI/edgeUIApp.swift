dc03c8e9e5edb41ffc32d5f8bbf66bc943ddfa0a
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-14 22:46:45 +0300

    Initial Commit

---
+++ edgeUI/edgeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  edgeUIApp.swift
+//  edgeUI
+//
+//  Created by Ahmad Yasser on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct edgeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
